42ec1556f65f2398341b40eb0cd961e9a23ce7ea
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-06-16 20:21:43 +0200

    UI finished

--- MissingPeople/ViewController.swift
+++ MissingPeople/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var selectedImg: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
-
+    @IBAction func checkForMatch(sender: AnyObject) {
+    }
 }
 
